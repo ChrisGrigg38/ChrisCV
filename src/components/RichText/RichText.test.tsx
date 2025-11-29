c8a4fdbce21473446b40a3ece6f9cadefc6af5ae
ChrisGrigg38 <crgrigg29@hotmail.com>
2025-11-29 16:21:17 +1100

    Fix unused ref error

--- src/components/RichText/RichText.test.tsx
+++ src/components/RichText/RichText.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import RichText from './RichText'
 
 describe('render RichText Component', () => {
